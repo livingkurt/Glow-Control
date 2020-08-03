5c21f84769f19384ec358affbc652ab5f3cca45d
Kurt LaVacque <lavacquek@icloud.com>
2020-08-02 20:58:40 -0500

    Finally connected the values of the number and range inputs

--- client/src/components/ColorComponents/SettingSlider.jsx
+++ client/src/components/ColorComponents/SettingSlider.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 const SettingSlider = (props) => {
+	const [ value, set_value ] = useState(props.setting.value);
 	return (
 		<div className="m-v-s">
 			<label className="m-t-s" htmlFor={props.setting.name}>
@@ -12,31 +13,33 @@ const SettingSlider = (props) => {
 					min={props.setting.min}
 					max={props.setting.max}
 					step={props.setting.step}
-					defaultValue={props.setting.value}
+					value={value}
 					className="w-6rem m-r-l"
 					name={props.setting.name}
 					onMouseUp={(e) => props.update_function(e.target.name, e.target.value)}
-					onChange={(e) =>
-						props.set_settings({
-							...props.settings,
-							[props.setting.name]: { ...props.settings[props.setting.name], value: e.target.value }
-						})}
+					// onChange={(e) =>
+					// 	props.set_settings({
+					// 		...props.settings,
+					// 		[props.setting.name]: { ...props.settings[props.setting.name], value: e.target.value }
+					// 	})}
+					onChange={(e) => set_value(e.target.value)}
 				/>
 				<input
 					type="range"
 					min={props.setting.min}
 					max={props.setting.max}
 					step={props.setting.step}
-					defaultValue={props.setting.value}
+					value={value}
 					dir={props.direction}
 					className="w-90"
 					name={props.setting.name}
 					onMouseUp={(e) => props.update_function(e.target.name, e.target.value)}
-					onChange={(e) =>
-						props.set_settings({
-							...props.settings,
-							[props.setting.name]: { ...props.settings[props.setting.name], value: e.target.value }
-						})}
+					// onChange={(e) =>
+					// 	props.set_settings({
+					// 		...props.settings,
+					// 		[props.setting.name]: { ...props.settings[props.setting.name], value: e.target.value }
+					// 	})}
+					onChange={(e) => set_value(e.target.value)}
 				/>
 			</div>
 		</div>
