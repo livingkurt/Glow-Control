cc9203e3ab3d97c7ada96fa8e03abcfcc3e0293d
Kurt LaVacque <lavacquek@icloud.com>
2020-08-05 08:55:21 -0500

    Uninstalled the cors thing

--- server.ts
+++ server.ts
@@ -20,8 +20,8 @@ mongoose
 	.catch((error: { reason: any }) => console.log(error.reason));
 
 const app = express();
-const cors = require('cors');
-app.use(cors());
+// const cors = require('cors');
+// app.use(cors());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
